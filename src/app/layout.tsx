32a9af9cc61fc6d8d33d9d9c4823eb47ea6358ee
AsgurDjons <asgyrdjons@gmail.com>
2024-08-10 15:51:36 +0300

    feat: Добавление препроцессора для стилей замена дефолтный файлов и иконок.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+import './globals.scss';
 import type { ReactNode } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -16,7 +16,7 @@ const RootLayout = ({
   children: ReactNode;
 }>) => {
   return (
-    <html lang="en">
+    <html lang="ru">
       <body className={inter.className}>{children}</body>
     </html>
   );
